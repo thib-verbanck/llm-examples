c353a889f9e91f7d8b29380a59c563e405beaa91
thib-verbanck <79321453+thib-verbanck@users.noreply.github.com>
2024-03-07 16:26:49 +0000

    test

---
+++ pages/CompanyGPT.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import anthropic
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("Chat with your files")
+uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
+question = st.text_input(
+    "Ask something about the article",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+
+if uploaded_file and question and not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+
+if uploaded_file and question and openai_api_key:
+    article = uploaded_file.read().decode()
+    prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n<article>
+    {article}\n\n</article>\n\n{question}{anthropic.AI_PROMPT}"""
+
+    client = anthropic.Client(api_key=openai_api_key)
+    response = client.completions.create(
+        prompt=prompt,
+        stop_sequences=[anthropic.HUMAN_PROMPT],
+        model="claude-v1",  # "claude-2" for Claude 2 model
+        max_tokens_to_sample=100,
+    )
+    st.write("### Answer")
+    st.write(response.completion)
